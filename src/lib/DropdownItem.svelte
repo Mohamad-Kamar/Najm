989a7d92ba900fd0c9086c6176f637cb90e76799
mkamar <mohamad.kamar@agoda.com>
2024-02-18 21:01:24 +0700

    Simplify dropdown item logic

--- src/lib/DropdownItem.svelte
+++ src/lib/DropdownItem.svelte
@@ -15,7 +15,6 @@
 	aria-label="Dropdown menu"
 	tabindex="0"
 >
-	<!-- Added tabindex -->
 	<button
 		class="hover:underline text-white bg-blue-500 px-4 py-2 rounded-md w-full h-full"
 		aria-haspopup="true"
@@ -24,21 +23,18 @@
 		{item.name}
 	</button>
 	{#if isOpen && item.items && item.items.length > 0}
-		<div
+		<ul
 			class={`absolute ${dropDirection === 'bottom' ? 'top-full mt-2' : 'left-full top-0'} bg-white shadow-md w-48`}
 			on:mouseenter={() => (isOpen = true)}
 			on:mouseleave={() => (isOpen = false)}
 			role="menu"
 			tabindex="0"
 		>
-			<!-- Added tabindex -->
-			<ul class="bg-blue-500">
-				{#each item.items as subItem}
-					<li class="hover:bg-blue-700 hover:text-white p-2 text-blue-500 bg-white" role="menuitem">
-						<DropdownItem item={subItem} dropDirection="left-full" />
-					</li>
-				{/each}
-			</ul>
-		</div>
+			{#each item.items as subItem}
+				<li class="hover:bg-blue-700 hover:text-white p-2 text-blue-500 bg-white" role="menuitem">
+					<DropdownItem item={subItem} dropDirection="left-full" />
+				</li>
+			{/each}
+		</ul>
 	{/if}
 </div>
