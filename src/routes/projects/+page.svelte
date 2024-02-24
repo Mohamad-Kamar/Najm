622b18ec058f1cf13c79ea29dba82327ed9e5178
mkamar <mohamad.kamar@agoda.com>
2024-02-24 21:28:04 +0700

    Add sort by projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
+	import { TabGroup, TabAnchor, ConicGradient } from '@skeletonlabs/skeleton';
+	import type { ConicStop } from '@skeletonlabs/skeleton';
+
+	const conicStops: ConicStop[] = [
+		{ color: 'rgba(255,255,255,1)', start: 0, end: 10 },
+		{ color: 'rgba(255,255,255,0.5)', start: 10, end: 35 },
+		{ color: 'rgba(255,255,255,0.25)', start: 35, end: 100 },
+	];
 </script>
 
 <TabGroup justify="justify-between">
-	<TabAnchor href="/"><span>Sort By Projects</span></TabAnchor>
+	<TabAnchor href="/projects/sort-by-projects"><span>Sort By Projects</span></TabAnchor>
 	<TabAnchor href="/about">Sort By Expenses/Revenues</TabAnchor>
 	<TabAnchor href="/about">Import Data</TabAnchor>
 	<TabAnchor href="/about">Share</TabAnchor>
 </TabGroup>
+<ConicGradient stops={conicStops}>(caption)</ConicGradient>
---
+++ src/routes/projects/sort-by-projects/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
+</script>
+
+<TabGroup justify="justify-evenly">
+	<TabAnchor href="/projects/sort-by-projects"><span>Timeline</span></TabAnchor>
+	<TabAnchor href="/about">Calendar</TabAnchor>
+	<TabAnchor href="/about">Workflow</TabAnchor>
+	<TabAnchor href="/about">Files</TabAnchor>
+</TabGroup>
