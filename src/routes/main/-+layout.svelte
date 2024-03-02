ba2eafcf07c5e3c69e7d308f46dd3145cc940d13
mkamar <mohamad.kamar@agoda.com>
2024-03-02 21:54:23 +0700

    Create the pages under the main page

---
+++ src/routes/main/-+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
---
+++ src/routes/main/inventory/+page.svelte
@@ -0,0 +1,74 @@
+<script>
+	import { AppShell } from '@skeletonlabs/skeleton';
+	let tableArr = [
+		{ position: 1, name: 'Hydrogen', symbol: 'H', weight: 1.0079 },
+		{ position: 2, name: 'Helium', symbol: 'He', weight: 4.0026 },
+		{ position: 3, name: 'Lithium', symbol: 'Li', weight: 6.941 },
+		{ position: 4, name: 'Beryllium', symbol: 'Be', weight: 9.0122 },
+		{ position: 5, name: 'Boron', symbol: 'B', weight: 10.811 },
+		{ position: 6, name: 'Carbon', symbol: 'C', weight: 12.0107 },
+		{ position: 7, name: 'Nitrogen', symbol: 'N', weight: 14.0067 },
+		{ position: 8, name: 'Oxygen', symbol: 'O', weight: 15.9994 },
+		{ position: 9, name: 'Fluorine', symbol: 'F', weight: 18.9984 },
+		{ position: 10, name: 'Neon', symbol: 'Ne', weight: 20.1797 },
+	];
+	const totalWeight = tableArr.reduce((acc, row) => acc + row.weight, 0);
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<div class="bg-gradient-theme h-32 w-full text-white">Header</div>
+	</svelte:fragment>
+	<div class="grid grid-cols-1 divide-y">
+		<div class="grid grid-cols-2 divide-x">
+			<div>
+				<div>
+					<label> From Date </label>
+				</div>
+				<div>
+					<label> FromTo Date </label>
+				</div>
+				<div>
+					<label> Search Item </label>
+				</div>
+			</div>
+			<div>
+				<div>
+					<label> Filter by </label>
+				</div>
+			</div>
+		</div>
+		<div>
+			<!-- Responsive Container (recommended) -->
+			<div class="table-container">
+				<!-- Native Table Element -->
+				<table class="table table-hover">
+					<thead>
+						<tr>
+							<th>Position</th>
+							<th>Name</th>
+							<th>Symbol</th>
+							<th>Weight</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each tableArr as row, i}
+							<tr>
+								<td>{row.position}</td>
+								<td>{row.name}</td>
+								<td>{row.symbol}</td>
+								<td>{row.weight}</td>
+							</tr>
+						{/each}
+					</tbody>
+					<tfoot>
+						<tr>
+							<th colspan="3">Calculated Total Weight</th>
+							<td>{totalWeight}</td>
+						</tr>
+					</tfoot>
+				</table>
+			</div>
+		</div>
+	</div>
+</AppShell>
