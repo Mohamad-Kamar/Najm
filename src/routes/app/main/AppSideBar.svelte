f38bbf3d8cb1f5d74b23b60b6c7ffb594fdb5df9
mkamar <mohamad.kamar@agoda.com>
2024-03-09 22:26:39 +0700

    Set sidebar buttons as code

--- src/routes/app/main/AppSideBar.svelte
+++ src/routes/app/main/AppSideBar.svelte
@@ -9,17 +9,40 @@
 			<img class="h-40 w-40 object-fill" src="/najm-logo.png" alt="Najm Logo" />
 		</AppRailAnchor>
 	</svelte:fragment>
-	<!-- --- -->
-	<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+	<AppRailAnchor
+		href="/app/main/inventory"
+		bind:group={currentTile}
+		name="tile-1"
+		value={0}
+		title="tile-1"
+	>
 		<span>Inventory</span>
-	</AppRailTile>
-	<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+	</AppRailAnchor>
+	<AppRailAnchor
+		href="/app/main/business-partners"
+		bind:group={currentTile}
+		name="tile-2"
+		value={1}
+		title="tile-2"
+	>
 		<span>Business Partners</span>
-	</AppRailTile>
-	<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+	</AppRailAnchor>
+	<AppRailAnchor
+		href="/app/main/financial-boxes"
+		bind:group={currentTile}
+		name="tile-3"
+		value={2}
+		title="tile-3"
+	>
 		<span>Financial Boxes</span>
-	</AppRailTile>
-	<AppRailTile bind:group={currentTile} name="tile-3" value={3} title="tile-3">
+	</AppRailAnchor>
+	<AppRailAnchor
+		href="/app/main/projects"
+		bind:group={currentTile}
+		name="tile-3"
+		value={3}
+		title="tile-3"
+	>
 		<span>Projects</span>
-	</AppRailTile>
+	</AppRailAnchor>
 </AppRail>
