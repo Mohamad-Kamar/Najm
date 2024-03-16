202258c9cac8dc893864d56be3e2a230a4dea28c
mkamar <mohamad.kamar@agoda.com>
2024-03-16 14:36:34 +0700

    Fix pages layout

--- src/routes/app/main/+layout.svelte
+++ src/routes/app/main/+layout.svelte
@@ -8,14 +8,18 @@
 	<AppShell>
 		<svelte:fragment slot="sidebarLeft"><AppSideBar /></svelte:fragment>
 		<svelte:fragment slot="pageHeader">
-			<div class="flex flex-row">
-				<div class="flex-initial">Financial Operations</div>
-				<div class="flex-initial">Invoicing</div>
-				<div class="flex-initial">Reports</div>
-				<div class="flex-initial">Purchacing</div>
-				<div class="flex-initial">Chat History</div>
-			</div>
+			<nav class="h-16 bg-surface-500 text-secondary-500">
+				<ul class="flex h-16 flex-row justify-evenly">
+					<li class="variant-filled btn rounded">Financial Operations</li>
+					<li class="variant-filled btn rounded">Invoicing</li>
+					<li class="variant-filled btn rounded">Reports</li>
+					<li class="variant-filled btn rounded">Purchacing</li>
+					<li class="variant-filled btn rounded">Chat History</li>
+				</ul>
+			</nav>
 		</svelte:fragment>
-		<slot />
+		<section class="container mx-auto p-8">
+			<slot />
+		</section>
 	</AppShell>
 </div>
